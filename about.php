ee2b4c3dd984d42bb05211c8553d1a0b2f936b10
Vishnumrvs <149359529+Vishnumrvs@users.noreply.github.com>
2023-12-20 00:09:45 -0800

    Update and rename about.html to about.php

---
+++ about.php
@@ -0,0 +1,31 @@
+<?php
+include 'navbar.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+</head>
+<body>
+    <h1>Here you can know more about us</h1>
+    <div id="content">
+    
+        <p id="description">Welcome to e-petshop! We provide a wide range of pet products and accessories to meet the needs of all pet owners.</p>
+        
+        <h2>Our Mission</h2>
+        <p>our mission is to help you care for your beloved companions with the highest-quality products and exceptional customer service.<br>
+          With a wide range of products for cats, dogs, birds, fish, and small animals, we aim to be your one-stop shop for all things pet-related. <br>Whether you're looking for nutritious food,  stimulating toys, stylish accessories, or essential healthcare products, we've got you covered.<br>We hand-select each item in our inventory to ensure that it meets our strict standards for quality, safety, and sustainability, 
+          <br>so you can shop with confidence knowing that you're giving your pets the very best. </p><br><br><br><br><br><br><br>
+        
+        <h2>Why Choose Us?</h2>
+        <ul>
+            <li>We offer high-quality products at competitive prices.</li>
+            <li>Our team is knowledgeable and passionate about pets.</li>
+            <li>We provide excellent customer service.</li>
+            <li>We have a wide selection of products to choose from.</li>
+        </ul>
+    </div>
+</body>
+</html>
